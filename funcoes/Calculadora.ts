3268ad6e0b9da4c30c380a9791a77dc8fca8feb4
hrvieira <luizhrvieira@outlook.com>
2024-03-26 18:03:11 -0300

    Refatoração do código para melhor legibilidade

--- funcoes/Calculadora.ts
+++ funcoes/Calculadora.ts
@@ -6,6 +6,13 @@ let num2: number;
 num1 = readline.questionFloat("Digite o primeiro número:\n");
 num2 = readline.questionFloat("Digite o segundo número:\n");
 
+console.log(`Soma: ${somar(num1,num2)}`);
+console.log(`Subtrai: ${subtrair(num1,num2)}`);
+console.log(`Multiplica: ${multiplicar(num1,num2)}`);
+console.log(`Divide: ${dividir(num1,num2)}`);
+
+about();
+
 function somar(num1: number, num2: number): number {
      return num1 + num2;
 }
@@ -22,14 +29,6 @@ function dividir(num1: number, num2: number): number {
      return num1 / num2;
 }
 
-
-console.log(`Soma: ${somar(num1,num2)}`);
-console.log(`Subtrai: ${subtrair(num1,num2)}`);
-console.log(`Multiplica: ${multiplicar(num1,num2)}`);
-console.log(`Divide: ${dividir(num1,num2)}`);
-
 function about(): void{
      console.log("Feito por hrvieira!")
 }
-
-about();
