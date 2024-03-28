3fe2e12582ca63ce104b09d08cb195404b3950ed
hrvieira <luizhrvieira@outlook.com>
2024-03-28 18:06:50 -0300

    Trabalhando com abstract usando clone de triatleta

---
+++ colecoes/ColecaoArray.ts
@@ -0,0 +1,5 @@
+let numeros= [10, ]
+
+
+// deleta - o valor 10 é encotrado em um array e um elemento é deletado, nesse caso o 10
+numeros.splice(numeros.indexOf(10), 1);
---
+++ triatleta_ts/Corredor.ts
@@ -0,0 +1,16 @@
+﻿import { Atleta } from "./Atleta";
+
+export class Corredor extends Atleta {
+    
+    constructor (nome: string){
+        super(nome);
+    }
+    
+    public correr(): void{
+        console.log("Correndo...");
+    }
+
+    public aquecer(): void {
+        console.log("Aquecendo na pista...")
+    }
+}
